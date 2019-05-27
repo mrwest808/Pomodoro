d0930b39503a9ed0c640dc1a3387c62847c707d0
Johan <johan.west@viablelab.com>
2019-05-27 13:50:19 +0200

    Initial Commit

---
+++ Pomodoro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pomodoro
+//
+//  Created by Johan West on 2019-05-27.
+//  Copyright © 2019 Johan West. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Johan West on 2019-05-27.
+//  Copyright © 2019 Johan West. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
